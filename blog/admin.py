1f04010c1d1d62dad3beaea4be96ed57e60b9de4
katyaga <aeva_ev@mail.ru>
2018-05-29 16:36:52 +0700

    Modified templates to display posts from database.

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_date', 'published_date')
+
+
+# admin.site.register(Post)
 
 
